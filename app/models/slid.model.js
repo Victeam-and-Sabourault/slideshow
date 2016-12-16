b169b6e0ead9f3742461e85156d7990e67a1a8c0
Adrien REDON <redon.adrien@gmail.com>
2016-12-16 09:51:40 +0100

    ADD: SlidModel CRUD methods

--- app/models/slid.model.js
+++ app/models/slid.model.js
@@ -1,33 +1,89 @@
-'use strict';
+"use strict";
+let fs = require('fs');
+let CONFIG = require("../../config.json");
 
-export default class SlidModel {
 
-    constructor (slid) {
-        this.type = slid.type;
-        this.id = slid.id;
-        this.title = slid.title;
-        this.fileName = slid.fileName;
-        let _data = slid.getData();
+class SlideModel {
 
-        this.getData = () => _data;
+    constructor () {
+        this.type;
+        this.id;
+        this.title;
+        this.fileName;
 
-        this.setData = (data) => _data = data; 
+        let _data;
+        this.setData = function(data) { 
+            _data = data; 
+        }
+        this.getData = function() { 
+            return _data; 
+        }
     }
 
-    static create (slid, callback) {
-
+    /* Prend un objet slidModel en paramètre, stocke le contenu de [slid.data] dans le fichier [slid.fileName] et stocke les metadonnées dans un fichier [slidModel.id].meta.json dans le répertoire [CONFIG.contentDirectory]. */
+    static create(slide, callback) {
+        if (slide instanceof SlideModel) {
+            if (slide.getData() !== null && slide.getData().length > 0 && slide.id !== null) {
+                fs.writeFile(CONFIG.contentDirectory + '/' + slide.id + ".meta.json", JSON.stringify(slide));
+                fs.writeFile(CONFIG.contentDirectory + '/' + slide.fileName, slide.getData());
+                callback();
+            } else {
+                callback("Problem in datas !");
+            }
+        } else {
+            callback("Not an instance of SlideModel !");
+        }   
     }
 
-    static read (id, callback) {
+    /* Prend un id en paramètre et retourne l’objet slidModel lu depuis le fichier [slid.id].meta.json */ 
+    static read(id, callback) {
+        if (id !== null) {
+            fs.readdir(CONFIG.contentDirectory, (err, files) => {
+                let file = files.filter((file) => file === id + ".meta.json")[0];
+                let fileContent = JSON.parse(fs.readFileSync(CONFIG.contentDirectory + '/' + file).toString());
+                let model = new SlideModel();
+                model.type = fileContent.type;
+                model.id = fileContent.id;
+                model.title = fileContent.title;
+                model.fileName = fileContent.fileName;
 
+                callback(null, model);
+            });
+        } else {
+            callback("Problem in datas !", null);
+        }
     }
 
-    static update (slid, callback) {
-
+    /* Prend l’id d’un SlidModel en paramètre et met à jour le fichier de metadata ([slid.id].meta.json) et le fichier [slid.fileName] si [slid.data] est renseigné (non nul avec une taille > 0). */
+    static update(slide, callback) {
+        if (slide.getData() !== null && slide.getData().length > 0 && slide.id !== null) {
+            if (fs.existsSync(CONFIG.contentDirectory + '/' + slide.id + ".meta.json") &&
+                fs.existsSync(CONFIG.contentDirectory + '/' + slide.id + ".meta.json")) {
+                    fs.writeFile(CONFIG.contentDirectory + '/' + slide.id + ".meta.json", JSON.stringify(slide));
+                    fs.writeFile(CONFIG.contentDirectory + '/' + slide.fileName, slide.getData());
+                    callback();
+                } else {
+                    callback("File doesn't exists !");
+                }
+        } else {
+            callback("Problem in datas !");
+        }
     }
 
-    static delete (id, callback) {
-
+    /* supprime les fichiers data ([slid.src]) et metadata ([slid.id].meta.json) */
+    static delete(id, callback) {
+        this.read(id, (err, data) => {
+            if (err) {
+                callback(err);
+            } else {
+                fs.unlink(CONFIG.contentDirectory + "/" + data.fileName, (err) => {
+                  fs.unlink(CONFIG.contentDirectory + "/" + id + ".meta.json", (err) => {
+                      callback();
+                    });
+                });
+            }
+        });
     }
-
 }
+
+module.exports = SlideModel;
