2a64daf4cf6dd9524a18c033d73c758afa61553c
Nicolas Lebrun <nicolas.lebrun@cpe.fr>
2017-01-08 12:05:55 +0100

    FUCKING 💩

--- app/controllers/io.controller.js
+++ app/controllers/io.controller.js
@@ -2,11 +2,15 @@ const socketIO = require('socket.io');
 const SlidModel = require('../models/slid.model');
 
 class IOController {
-    constructor () {
+
+    constructor() {
         this.mapSocket;
     }
 
-    listen (server) {
+    listen(server) {
+
+        let presId;
+        let slidId;
         let io = socketIO.listen(server);
         io.sockets.on('connection', (socket, idClient) => {
             // send event connection to client
@@ -17,8 +21,32 @@ class IOController {
 
             // listen to slid event
             socket.on('slidEvent', message => {
+
+                switch (message.CMD) {
+                    case 'START':
+                        presId = message.presId;
+                        break;
+                    case 'BEGIN':
+                        if (fs.existsSync("./" + CONFIG.presentationDirectory + "/" + request.presId + ".pres.json")) {
+                            fs.readFile("./" + CONFIG.presentationDirectory + "/" + request.presId + ".pres.json", 'utf8', function (err, data) {
+                                let pres = JSON.parse(data.toString());
+                                slidId = pres.slidArray[0].id;
+                            });
+                        }
+                        break;
+                    case 'NEXT':
+                        break;
+                    case 'PREV':
+                        break;
+                    case 'PAUSE':
+                        break;
+                    case 'END':
+                        break;
+                    default:
+                }
+
                 if (message.CMD !== 'PAUSE') {
-                    SlidModel.read(message.PRES_ID, (err, slid) => {
+                    SlidModel.read(slidId, (err, slid) => {
                         slid.src = '/slid/' + slid.id;
                         socket.broadcast.emit('slid', slid);
                     });
--- public/admin/js/services/CommService.js
+++ public/admin/js/services/CommService.js
@@ -1,58 +1,59 @@
-angular.module('commServices', []).factory('comm',commFnc);
-commFnc.$inject=['$http','$q'];
+angular.module('commServices', []).factory('comm', commFnc);
+commFnc.$inject = ['$http', '$q'];
 
-function commFnc($http,$q ){
+function commFnc($http, $q) {
 
-     var comm = {
-         loadImages: loadImages,
-         loadAllImages: loadAllImages,
-         uploadImage: uploadImage,
-         loadPres: loadPres,
-         savePres: savePres
-     };
-   
-   function loadImages(presID) { 
-       var deferred = $q.defer();
-       $http.get('/pres/'+presID+'/images')
+    var comm = {
+        loadImages: loadImages,
+        loadAllImages: loadAllImages,
+        uploadImage: uploadImage,
+        loadPres: loadPres,
+        savePres: savePres,
+        io: io
+    };
+
+    function loadImages(presID) {
+        var deferred = $q.defer();
+        $http.get('/pres/' + presID + '/images')
             .then(
-                (data, status, headers, config) => deferred.resolve(data))
+            (data, status, headers, config) => deferred.resolve(data))
             .catch(
-                (data, status, headers, config) => deferred.reject(status));
-       return deferred.promise;
+            (data, status, headers, config) => deferred.reject(status));
+        return deferred.promise;
     }
 
     function loadAllImages() {
         var deferred = $q.defer();
-       $http.get('/uploads')
+        $http.get('/uploads')
             .then(
-                (data, status, headers, config) => deferred.resolve(data))
+            (data, status, headers, config) => deferred.resolve(data))
             .catch(
-                (data, status, headers, config) => deferred.reject(status));
-       return deferred.promise;
+            (data, status, headers, config) => deferred.reject(status));
+        return deferred.promise;
     }
 
-    function loadPres(presID) { 
+    function loadPres(presID) {
         var deferred = $q.defer();
-        $http.get('/loadPres/'+presID)
+        $http.get('/loadPres/' + presID)
             .then(
-                (data, status, headers, config) => deferred.resolve(data))
+            (data, status, headers, config) => deferred.resolve(data))
             .catch(
-                (data, status, headers, config) => deferred.reject(status));
+            (data, status, headers, config) => deferred.reject(status));
         return deferred.promise;
     }
 
     function uploadImage(presID, slidID, file) {
         var deferred = $q.defer();
-       $http.post(`/pres/${presID}/slid/${slidID}/upload/${file}`)
+        $http.post(`/pres/${presID}/slid/${slidID}/upload/${file}`)
             .then(
-                (data, status, headers, config) => deferred.resolve(data))
+            (data, status, headers, config) => deferred.resolve(data))
             .catch(
-                (data, status, headers, config) => deferred.reject(status));
-       return deferred.promise;
+            (data, status, headers, config) => deferred.reject(status));
+        return deferred.promise;
     }
 
-     //TODO
-   function savePres(pres) { 
+    //TODO
+    function savePres(pres) {
         // TODO
         var deferred = $q.defer();
         setTimeout(() => {
@@ -61,7 +62,46 @@ function commFnc($http,$q ){
         return deferred.promise;
     }
 
+    comm.io = {};
+    comm.io.socketConnection = function (scope, uuid) {
+        var socket = io.connect();
+        comm.io.uuid = uuid;
+        socket.on('connection', function () {
+            socket.emit('data_comm', { 'id': comm.io.uuid });
+        });
+        socket.on('newPres', function (socket) {
+
+        });
+        socket.on('slidEvent', function (socket) {
+
+        });
+        return socket;
+    }
+
+    comm.io.emitPrev = function (socket) {
+        socket.emit('slidEvent', { 'CMD': "PREV" });
+    }
+
+    comm.io.emitNext = function (socket) {
+        socket.emit('slidEvent', { 'CMD': "NEXT" });
+    }
+
+    comm.io.emitStart = function (socket, presUUID) {
+        socket.emit('slidEvent', { 'CMD': "START", 'PRES_ID': presUUID });
+    }
+
+    comm.io.emitPause = function (socket) {
+        socket.emit('slidEvent', { 'CMD': "PAUSE" });
+    }
+
+    comm.io.emitBegin = function (socket) {
+        socket.emit('slidEvent', { 'CMD': "BEGIN" });
+    }
+
+    comm.io.emitEnd = function (socket) {
+        socket.emit('slidEvent', { 'CMD': "END" });
+    }
+
     return comm;
-   
+
 }
-    
