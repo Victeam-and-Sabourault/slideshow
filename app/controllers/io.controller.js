6d8b2a0cf0ea6ef3e2bff2ee34181eebcf7d98a2
Adrien REDON <redon.adrien@gmail.com>
2017-01-08 12:21:07 +0100

    FIX: socket IO server side

--- app/controllers/io.controller.js
+++ app/controllers/io.controller.js
@@ -10,7 +10,7 @@ class IOController {
     listen(server) {
 
         let presId;
-        let slidId;
+        let slidPos;
         let io = socketIO.listen(server);
         io.sockets.on('connection', (socket, idClient) => {
             // send event connection to client
@@ -22,6 +22,32 @@ class IOController {
             // listen to slid event
             socket.on('slidEvent', message => {
 
+                if (fs.existsSync("./" + CONFIG.presentationDirectory + "/" + request.presId + ".pres.json")) {
+                            fs.readFile("./" + CONFIG.presentationDirectory + "/" + request.presId + ".pres.json", 'utf8', function (err, data) {
+                                let pres = JSON.parse(data.toString());
+                                switch (message.CMD) {
+                                    case 'START':
+                                        presId = message.PRES_ID;
+                                    case 'BEGIN':
+                                        slidPos = 0;
+                                        break;
+                                    case 'NEXT':
+                                        slidPos++;
+                                        break;
+                                    case 'PREV':
+                                        slidPos--;
+                                        break;
+                                    case 'END':
+                                        slidPos = pres.slidArray.length - 1;
+                                        break;
+                                    default:
+                                        break;
+                                }
+                                slidId = pres.slidArray[slidPos].id;
+                                console.log(slidId);
+                            });
+                        }
+
                 switch (message.CMD) {
                     case 'START':
                         presId = message.presId;
@@ -31,25 +57,42 @@ class IOController {
                             fs.readFile("./" + CONFIG.presentationDirectory + "/" + request.presId + ".pres.json", 'utf8', function (err, data) {
                                 let pres = JSON.parse(data.toString());
                                 slidId = pres.slidArray[0].id;
+                                console.log(slidId)
                             });
                         }
                         break;
                     case 'NEXT':
+                        if (fs.existsSync("./" + CONFIG.presentationDirectory + "/" + request.presId + ".pres.json")) {
+                            fs.readFile("./" + CONFIG.presentationDirectory + "/" + request.presId + ".pres.json", 'utf8', function (err, data) {
+                                let pres = JSON.parse(data.toString());
+                                slidId = pres.slidArray[++slidId].id;
+                                console.log(slidId)
+                            });
+                        }
                         break;
                     case 'PREV':
                         break;
                     case 'PAUSE':
                         break;
                     case 'END':
+                        if (fs.existsSync("./" + CONFIG.presentationDirectory + "/" + request.presId + ".pres.json")) {
+                            fs.readFile("./" + CONFIG.presentationDirectory + "/" + request.presId + ".pres.json", 'utf8', function (err, data) {
+                                let pres = JSON.parse(data.toString());
+                                slidId = pres.slidArray[pres.slidArray.length - 1].id;
+                                console.log(slidId)
+                            });
+                        }
                         break;
                     default:
                 }
 
                 if (message.CMD !== 'PAUSE') {
-                    SlidModel.read(slidId, (err, slid) => {
-                        slid.src = '/slid/' + slid.id;
-                        socket.broadcast.emit('slid', slid);
-                    });
+                    setTimeout(() => {
+                        SlidModel.read(slidId, (err, slid) => {
+                            slid.src = '/slid/' + slid.id;
+                            socket.broadcast.emit('slid', slid);
+                        });
+                    }, 1000);
                 }
             });
         });
